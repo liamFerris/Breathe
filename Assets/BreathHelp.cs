ae9182a81c49b5f6db1661894bd92691cbb7e32a
unknown <L.ferris@kainos.com name = Liam>
2017-09-18 18:52:08 +0100

    added bar

---
+++ Assets/BreathHelp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathHelp : MonoBehaviour
+{
+
+    public GameObject[] leaves;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float count = 0;
+        foreach(GameObject g in leaves)
+        {
+            if (g.transform.position.y < 0.5f)
+            {
+                count++;
+            }
+        }
+        float x = count / 151f;
+        Debug.Log(x);
+        transform.localScale = new Vector3(x, 0.25f, 0.1f);
+        if (count == 150)
+            Debug.Log("oh hey");
+	}
+}
